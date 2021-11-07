b19cf5a73f8060a9082443c93b9dd2efd12185f4
bfischtech(PC) <bfischtech@gmail.com>
2021-11-07 11:49:50 -0500

    transferservice complete assuming I didn't mess it up, program.cs needs done and presumably we're going to need some models in the client class?

--- dotnet/TenmoClient/TransferService.cs
+++ dotnet/TenmoClient/TransferService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Transactions;
 using TenmoClient.Exceptions;
 using TenmoClient.Models;
+using System.Collections.Generic;
 
 namespace TenmoClient
 {
@@ -16,9 +17,7 @@ namespace TenmoClient
 
         /* 
          * 
-        I am SUPER unsure about this SendTeBucks and haven't started on the rest, 
-        once the transferservice is built out we should just have to finish out the program.cs 
-        and then we'll be done  ... I think?
+        I hope this transferservice stuff is correct ... - bfisch
 
          */
         public string SendTEBucks()
@@ -38,7 +37,78 @@ namespace TenmoClient
             return null;
         }
 
-            private void ProcessErrorResponse(IRestResponse response)
+        public Transaction GetTransactionById(int id)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transactions/{id}");
+            request.AddUrlSegment("id", id);
+            IRestResponse<Transaction> response = client.Get<Transaction>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed || !response.IsSuccessful)
+            {
+                ProcessErrorResponse(response);
+            }
+            else
+            {
+                return response.Data;
+            }
+
+            return null;
+        }
+
+        public string RequestTeBucks()
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transactions/request");
+            IRestResponse<Transaction> response = client.Post<Transaction>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed || !response.IsSuccessful)
+            {
+                ProcessErrorResponse(response);
+            }
+            else
+            {
+                return response.StatusCode.ToString();
+            }
+
+            return null;
+        }
+
+        public List<Transaction> ViewPastTransfers(int id)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transactions/past");
+            request.AddUrlSegment("id", id);
+            IRestResponse<List<Transaction>> response = client.Get<List<Transaction>>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed || !response.IsSuccessful)
+            {
+                ProcessErrorResponse(response);
+            }
+            else
+            {
+                return response.Data;
+            }
+
+            return null;
+        }
+
+        public List<Transaction> ViewPendingTransfer(int id)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transactions/pending");
+            request.AddUrlSegment("id", id);
+            IRestResponse<List<Transaction>> response = client.Get<List<Transaction>>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed || !response.IsSuccessful)
+            {
+                ProcessErrorResponse(response);
+            }
+            else
+            {
+                return response.Data;
+            }
+
+            return null;
+        }
+
+        private void ProcessErrorResponse(IRestResponse response)
         {
             if (response.ResponseStatus != ResponseStatus.Completed)
             {
