86a68958b5ba2e53406119d1535b5cb5c56512fc
bfischtech(PC) <bfischtech@gmail.com>
2021-11-07 20:10:34 -0500

    split API classes back out

--- dotnet/TenmoClient/TransferService.cs
+++ dotnet/TenmoClient/TransferService.cs
@@ -8,100 +8,20 @@ using System.Transactions;
 
 using System.Collections.Generic;
 
-/* */
-
 namespace TenmoClient
 {
-    public class AuthService
+    public class TransferService
     {
         private readonly static string API_BASE_URL = "https://localhost:44315/";
         private readonly IRestClient client = new RestClient();
 
         //login endpoints
-        public bool Register(LoginUser registerUser)
-        {
-            RestRequest request = new RestRequest(API_BASE_URL + "login/register");
-            request.AddJsonBody(registerUser);
-            IRestResponse<ApiUser> response = client.Post<ApiUser>(request);
-
-            if (response.ResponseStatus != ResponseStatus.Completed)
-            {
-                Console.WriteLine("An error occurred communicating with the server.");
-                return false;
-            }
-            else if (!response.IsSuccessful)
-            {
-                if (!string.IsNullOrWhiteSpace(response.Data.Message))
-                {
-                    Console.WriteLine("An error message was received: " + response.Data.Message);
-                }
-                else
-                {
-                    Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
-                }
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        public ApiUser Login(LoginUser loginUser)
-        {
-            RestRequest request = new RestRequest(API_BASE_URL + "login");
-            request.AddJsonBody(loginUser);
-            IRestResponse<ApiUser> response = client.Post<ApiUser>(request);
-
-            if (response.ResponseStatus != ResponseStatus.Completed)
-            {
-                Console.WriteLine("An error occurred communicating with the server.");
-                return null;
-            }
-            else if (!response.IsSuccessful)
-            {
-                if (!string.IsNullOrWhiteSpace(response.Data.Message))
-                {
-                    Console.WriteLine("An error message was received: " + response.Data.Message);
-                }
-                else
-                {
-                    Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
-                }
-                return null;
-            }
-            else
-            {
-                client.Authenticator = new JwtAuthenticator(response.Data.Token);
-                return response.Data;
-            }
-        }
-
-        public decimal? GetBalance()
-        {
-            client.Authenticator = new JwtAuthenticator(UserService.GetToken());
-
-            RestRequest request = new RestRequest(API_BASE_URL + "balance");
-            IRestResponse<AccountBalance> response = client.Get<AccountBalance>(request);
-            
-            if (response.ResponseStatus != ResponseStatus.Completed || !response.IsSuccessful)
-            {
-                ProcessErrorResponse(response);
-            }
-            else
-            {
-                return response.Data.Balance;
-            }
-
-            return null;
-        }
+        
         public string SendTEBucks()
         {
-            client.Authenticator = new JwtAuthenticator(UserService.GetToken());
-
             RestRequest request = new RestRequest(API_BASE_URL + "transactions/send");
             IRestResponse<Transaction> response = client.Post<Transaction>(request);
-           
+            UserService.GetToken();
 
             if (response.ResponseStatus != ResponseStatus.Completed || !response.IsSuccessful)
             {
@@ -117,8 +37,6 @@ namespace TenmoClient
 
         public Transaction GetTransactionById(int id)
         {
-            client.Authenticator = new JwtAuthenticator(UserService.GetToken());
-
             RestRequest request = new RestRequest(API_BASE_URL + "transactions/{id}");
             request.AddUrlSegment("id", id);
             IRestResponse<Transaction> response = client.Get<Transaction>(request);
@@ -137,8 +55,6 @@ namespace TenmoClient
 
         public string RequestTeBucks()
         {
-            client.Authenticator = new JwtAuthenticator(UserService.GetToken());
-
             RestRequest request = new RestRequest(API_BASE_URL + "transactions/request");
             IRestResponse<Transaction> response = client.Post<Transaction>(request);
 
@@ -156,8 +72,6 @@ namespace TenmoClient
 
         public List<Transaction> ViewPastTransfers(int id)
         {
-            client.Authenticator = new JwtAuthenticator(UserService.GetToken());
-
             RestRequest request = new RestRequest(API_BASE_URL + "transactions");
             request.AddUrlSegment("id", id);
             IRestResponse<List<Transaction>> response = client.Get<List<Transaction>>(request);
@@ -176,8 +90,6 @@ namespace TenmoClient
 
         public List<Transaction> ViewPendingTransfer(int id)
         {
-            client.Authenticator = new JwtAuthenticator(UserService.GetToken());
-
             RestRequest request = new RestRequest(API_BASE_URL + "transactions");
             request.AddUrlSegment("id", id);
             IRestResponse<List<Transaction>> response = client.Get<List<Transaction>>(request);
